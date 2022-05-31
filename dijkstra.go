f741e44171365cf25a5e1de156fc2710e6661e3d
Adityapnn811 <72344203+Adityapnn811@users.noreply.github.com>
2022-05-31 13:20:03 +0700

    init commit and a little bit of defining project structure

---
+++ dijkstra.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func dijkstra() {
+	fmt.Println("Im calling dijkstra")
+}
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Set the router as the default one shipped with Gin
+	router := gin.Default()
+
+	// Serve frontend static files (Note: It will look for index.html under the frontend/ folder)
+	// Argumen pertama adalah route nya (untuk Serve)
+	router.Use(static.Serve("/", static.LocalFile("./views", true)))
+
+	// Setup route group for the API
+	api := router.Group("/api")
+	{
+		api.GET("/", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "pong",
+			})
+		})
+	}
+	// Route untuk web hanya ada dua, retrieve txt dan hitung dijkstra
+	api.GET("/getGraph", getGraphHandler)
+	api.POST("/countDijkstra", countDijkstraHandler)
+
+	dijkstra()
+	// Start and run the server
+	router.Run("localhost:3000")
+}
+
+func getGraphHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "getGraph",
+	})
+}
+
+func countDijkstraHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "countDijkstra",
+	})
+}
